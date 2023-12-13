6500d4123f114a69bc455bd4b983fd0c12e70a98
m_ahsan99 <m_ahsan99@hotmail.com>
2023-12-12 21:45:37 -0600

    Add project files.

---
+++ Controllers/Exchange-RatesController.cs
@@ -0,0 +1,86 @@
+﻿using ExchangeRate.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net;
+
+namespace ExchangeRate.Controllers
+{
+   
+    [ApiController]
+    [Route("api/exchange-rates")]
+
+    public class ExchangeRatesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+
+        private readonly ILogger<ExchangeRatesController> _logger;
+
+        public ExchangeRatesController(ILogger<ExchangeRatesController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        //[HttpGet(Name = "exchange-rates")]
+        //public List<PartnerRatesResponse> GetRates(string? country)
+        //{
+        //  List<PartnerRatesResponse> resp = new List<PartnerRatesResponse>();
+        //    Partners? p = Partners.GetListofPartners().Where(X => X.CountryCode == country).FirstOrDefault<Partners>();
+        //    if (p != null)
+        //    {
+        //        RootObject? ratesList = new RootObject();
+        //        ratesList = JsonConvert.DeserializeObject<RootObject>(JsonFile.jsonString);
+        //        if (ratesList != null)
+        //        {
+        //            List<PartnerRates> prList = ratesList.PartnerRates.Where(x => x.Currency == p.CurrencyCode).ToList();
+        //            foreach (PartnerRates r2 in prList)
+        //            {
+        //                resp.Add(new PartnerRatesResponse() { CountryCode = p.CountryCode,CurrencyCode = p.CurrencyCode,DeliveryMethod = r2.DeliveryMethod,PangeaRate = r2.Rate+p.FlatRate,PaymentMethod=r2.PaymentMethod });
+
+        //            }
+        //        }
+        //    }
+
+        //   // return null;
+        //    return resp;
+
+        //}
+
+
+        [HttpGet(Name = "exchange-rates")]
+        public IActionResult GetExchangeRates(string? country)
+        {
+            List<PartnerRatesResponse> resp = new List<PartnerRatesResponse>();
+            Partners? p = Partners.GetListofPartners().Where(X => X.CountryCode == country).FirstOrDefault<Partners>();
+            if (p != null)
+            {
+                RootObject? ratesList = new RootObject();
+                ratesList = JsonConvert.DeserializeObject<RootObject>(JsonFile.jsonString);
+                if (ratesList != null)
+                {
+                    List<PartnerRates> prList = ratesList.PartnerRates.Where(x => x.Currency == p.CurrencyCode).ToList();
+                    foreach (PartnerRates r2 in prList)
+                    {
+                        resp.Add(new PartnerRatesResponse() { CountryCode = p.CountryCode, CurrencyCode = p.CurrencyCode, DeliveryMethod = r2.DeliveryMethod, PangeaRate = r2.Rate + p.FlatRate, PaymentMethod = r2.PaymentMethod });
+
+                    }
+                }
+                else
+                {
+                    return NotFound("The exchange rate is not configured for the provided country.");
+                }
+            }
+            else
+            {
+                return NotFound("The Provided Country is not configured in the system.");             
+            }
+
+            // return null;
+            return Ok(resp);
+
+        }
+    }
+}
---
+++ Entities/Entities.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+
+namespace ExchangeRate.Entities
+{
+    internal class Partners
+    {
+        public string? Country { get; set; }
+        public string? CountryCode { get; set; }
+        public string? CurrencyCode { get; set; }
+        public decimal FlatRate { get; set; }
+
+        internal static List<Partners> GetListofPartners()
+        {
+               List<Partners> partners = new List<Partners>() 
+               {
+                   new Partners { Country = "Mexico",CountryCode="MXN" ,CurrencyCode = "MEX", FlatRate =  0.024m },
+                   new Partners { Country = "India",CountryCode="IND" ,CurrencyCode = "INR", FlatRate = 3.213m },
+                   new Partners { Country = "Philippines",CountryCode="PHL" ,CurrencyCode = "PHP", FlatRate = 2.437m },
+                   new Partners { Country = "Guatemala",CountryCode="GTM" ,CurrencyCode = "GTQ", FlatRate =  0.056m }
+
+                };
+
+            return partners;
+        }
+
+    }
+
+    internal class PartnerRates
+    {
+        [JsonProperty("Currency")]
+        public string? Currency { get; set; }
+        public string? PaymentMethod { get; set; }
+        public string? DeliveryMethod { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime AcquiredDate { get; set; }
+    }
+
+    public class PartnerRatesResponse
+    {
+        public string? CurrencyCode { get; set; }
+        public string? CountryCode { get; set; }
+        public decimal PangeaRate { get; set; }
+        public string? PaymentMethod { get; set; }
+        public string? DeliveryMethod { get; set; }
+    }
+    internal class JsonFile
+    {
+        public static string jsonString = "{\"PartnerRates\": [\r\n{\r\n\"Currency\": \"MXN\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"cash\",\r\n\"Rate\": 16.78,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"MXN\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"deposit\",\r\n\"Rate\": 16.83,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"MXN\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"debit\",\r\n\"Rate\": 16.78,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"MXN\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"cash\",\r\n\"Rate\": 16.65,\r\n\"AcquiredDate\": \"2023-07-26T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"MXN\",\r\n\"PaymentMethod\": \"bankaccount\",\r\n\"DeliveryMethod\": \"debit\",\r\n\"Rate\": 16.81,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"MXN\",\r\n\"PaymentMethod\": \"bankaccount\",\r\n\"DeliveryMethod\": \"deposit\",\r\n\"Rate\": 16.89,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"MXN\",\r\n\"PaymentMethod\": \"cash\",\r\n\"DeliveryMethod\": \"deposit\",\r\n\"Rate\": 16.79,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"MXN\",\r\n\"PaymentMethod\": \"bankaccount\",\r\n\"DeliveryMethod\": \"deposit\",\r\n\"Rate\": 16.89,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"PHP\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"cash\",\r\n\"Rate\": 57.14,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"PHP\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"deposit\",\r\n\"Rate\": 56.74,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"PHP\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"debit\",\r\n\"Rate\": 56.12,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"PHP\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"cash\",\r\n\"Rate\": 57.3,\r\n\"AcquiredDate\": \"2023-07-26T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"PHP\",\r\n\"PaymentMethod\": \"bankaccount\",\r\n\"DeliveryMethod\": \"debit\",\r\n\"Rate\": 57.33,\r\n\"AcquiredDate\": \"2023-07-26T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"PHP\",\r\n\"PaymentMethod\": \"bankaccount\",\r\n\"DeliveryMethod\": \"deposit\",\r\n\"Rate\": 57.79,\r\n\"AcquiredDate\": \"2023-07-26T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"PHP\",\r\n\"PaymentMethod\": \"bankaccount\",\r\n\"DeliveryMethod\": \"cash\",\r\n\"Rate\": 57.63,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"PHP\",\r\n\"PaymentMethod\": \"cash\",\r\n\"DeliveryMethod\": \"cash\",\r\n\"Rate\": 57.22,\r\n\"AcquiredDate\": \"2023-07-26T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"GTQ\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"deposit\",\r\n\"Rate\": 8.26,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"GTQ\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"debit\",\r\n\"Rate\": 8.08,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"GTQ\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"cash\",\r\n\"Rate\": 8.14,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"INR\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"cash\",\r\n\"Rate\": 81.72,\r\n\"AcquiredDate\": \"2023-07-26T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"INR\",\r\n\"PaymentMethod\": \"bankaccount\",\r\n\"DeliveryMethod\": \"debit\",\r\n\"Rate\": 81.85,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"INR\",\r\n\"PaymentMethod\": \"bankaccount\",\r\n\"DeliveryMethod\": \"deposit\",\r\n\"Rate\": 81.83,\r\n\"AcquiredDate\": \"2023-07-26T05:00:00.000Z\"\r\n},\r\n{\r\n\"Currency\": \"INR\",\r\n\"PaymentMethod\": \"debit\",\r\n\"DeliveryMethod\": \"cash\",\r\n\"Rate\": 81.63,\r\n\"AcquiredDate\": \"2023-07-24T05:00:00.000Z\"\r\n}\r\n]\r\n}";
+
+    }
+
+    internal class RootObject
+    {
+        public List<PartnerRates>? PartnerRates { get; set; }
+    }
+
+}
